49499be514bf36c3068a59049fa8da9cf82b6e46
nileshchouhan09 <chauhannilesh251@gmail.com>
2024-09-30 19:47:25 +0530

    fix styling

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -45,33 +45,26 @@ const Contact = () => {
       setLoading(false);
     }
 
-    // service_8keqwuj
+    
   };
   return (
     <section className="c-space my-20" id="contact">
       <p className="head-text"> Contact Me </p>
 
-      <div className=" relative min-h-screen flex items-center justify-center  mb-40 ">
-       {
-        isDesktop &&  <img
-        src="/assets/terminal.png"
-        alt="terminal"
-        className="absolute inset-0 min-h-screen "
-      />
-       }
+      <div className="relative min-h-screen flex items-center justify-center flex-col p-10 m-5 bg-[url('/assets/terminal.png')] bg-cover">
+       
 
-        <div className={` max-w-xl relative z-10 sm:px-10 px-5  ${isTablet }? mt-40 : mt-10 `}>
-          <h3 className="head-text"> Let's talk</h3>
-          <p className="text-lg  text-white-600">
-            {" "}
-            Whether you're looking to build a new website, improve your existing
-            platform, or bring a unique project to life, I'm here to help.
+        <div className="contact-container">
+          <h3 className="head-text">Let's talk</h3>
+          <p className="text-lg text-white-600 mt-3">
+            Whether you’re looking to build a new website, improve your existing platform, or bring a unique project to
+            life, I’m here to help.
           </p>
 
           <form
             ref={formRef}
             onSubmit={handleSubmit}
-            className="mt-7 flex flex-col space-y-8 "
+            className="mt-4 flex flex-col space-y-8 "
           >
             <label htmlFor="name">
               <span className="field-label">Full Name</span>
