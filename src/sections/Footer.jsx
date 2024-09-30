b643fc76dc8f0ce39e4549033975aae13cafd6b6
nileshchouhan09 <chauhannilesh251@gmail.com>
2024-09-30 17:56:05 +0530

    minor_fixes

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,32 +1,34 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <section className='c-space pt-7 pb-3 border-t border-black-300 flex  items-center justify-between flex-wrap gap-5' >
-
-      <div className='text-white-500 flex gap-2'>
+    <section className="c-space pt-7 mt-20 pb-3 border-t border-black-300 flex  items-center justify-between flex-wrap gap-5">
+      <div className="text-white-500 flex gap-2">
         <p> Terms & Conditions</p>
         <p>|</p>
         <p>Privacy Policy</p>
-
       </div>
 
-      <div className='flex gap-3 justify-center items-center'>
-        <div className='social-icons'>
-            <a href="https://github.com/nileshchouhan09" target='_blank'>
-            <img src="/assets/github.svg" alt="github" className='w-6 h-6' />
-            </a>
+      <div className="flex gap-3 justify-center items-center">
+        <div className="social-icons">
+          <a href="https://github.com/nileshchouhan09" target="_blank">
+            <img src="/assets/github.svg" alt="github" className="w-6 h-6" />
+          </a>
         </div>
-        <div className='social-icons'>
-            <a href="https://www.linkedin.com/in/nilesh-chouhan-491768216/" target='_blank'>
-            <img src="/assets/linkedin.png" alt="github" className='w-8 h-8' />
-            </a>
+        <div className="social-icons">
+          <a
+            href="https://www.linkedin.com/in/nilesh-chouhan-491768216/"
+            target="_blank"
+          >
+            <img src="/assets/linkedin.png" alt="github" className="w-8 h-8" />
+          </a>
         </div>
-       
       </div>
-      <p className='text-white-500'>© 2024 Nilesh Chauhan. All rights reserved.</p>
+      <p className="text-white-500">
+        © 2024 Nilesh Chauhan. All rights reserved.
+      </p>
     </section>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
